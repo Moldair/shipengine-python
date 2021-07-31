5d7210ddc05e7dbcea2a8c572a115fa05868be2d
Mickey McClellan <288259+Moldair@users.noreply.github.com>
2021-07-30 23:49:29 -0500

    added APIKey Acquisition and session setup

---
+++ shipengine/__init__.py
@@ -0,0 +1,19 @@
+import os
+import requests
+
+SHIP_ENGINE_API_KEY = os.environ.get('SHIP_ENGINE_API_KEY', None)
+
+class APIKeyMissingError(Exception):
+    pass
+
+if SHIP_ENGINE_API_KEY is None:
+    raise APIKeyMissingError(
+        "Usage of this wrapper requires and API"
+        "vist https://www.shipengine.com/docs/ to aquire an API Key."
+    )
+
+session = requests.Session()
+session.params = {}
+session.params['api_key'] = SHIP_ENGINE_API_KEY
+
+from .shipengine import ShipEngine
