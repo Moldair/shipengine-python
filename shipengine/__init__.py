2abce55e1164ff118c1b676dbcc184ee82fabd09
Mickey McClellan <288259+Moldair@users.noreply.github.com>
2021-08-01 00:14:48 -0500

    moved sessions to base class

--- shipengine/__init__.py
+++ shipengine/__init__.py
@@ -1,5 +1,4 @@
 import os
-import requests
 
 SHIP_ENGINE_API_KEY = os.environ.get('SHIP_ENGINE_API_KEY', None)
 SHIP_ENGINE_HOST = 'https://api.shipengine.com'
@@ -12,11 +11,6 @@ if SHIP_ENGINE_API_KEY is None:
         "Usage of this wrapper requires and API"
         "vist https://www.shipengine.com/docs/ to aquire an API Key."
     )
-
-session = requests.Session()
-session.params = {}
-session.params['api_key'] = SHIP_ENGINE_API_KEY
-
 
 # Status Codes
 SE_SUCCESS = 200	    # Success
--- shipengine/shipengine.py
+++ shipengine/shipengine.py
@@ -1,30 +1,43 @@
 
-from shipengine import session, SHIP_ENGINE_API_KEY, SHIP_ENGINE_HOST
-
+from shipengine import SHIP_ENGINE_API_KEY, SHIP_ENGINE_HOST
+import requests
+from pprint import pprint
 
 class ShipEngine():
         
     def __init__(self):
-        self.host = SHIP_ENGINE_HOST
+        self.url = SHIP_ENGINE_HOST
         self.headers = {
             'Host': 'api.shipengine.com',
             'API-Key': SHIP_ENGINE_API_KEY,
             'Content-Type': 'application/json',
         }
+                
+        self.session = requests.Session()
+        self.session.params = {}
+        self.session.params['api_key'] = SHIP_ENGINE_API_KEY
         return
     
     def put(self, url, data=None, json=None, **kwargs):
-        return session.put(url=url, data=data, json=json, headers=self.headers, **kwargs)
+        pprint(f"url: {url}")
+        pprint(f"data: {data}")
+        pprint(f"json: {json}")
+        pprint(f"headers: {self.headers}")
+        return self.session.put(url=url, data=data, json=json, headers=self.headers, **kwargs)
     
     def get(self, url, params=None, **kwargs):
-        return session.get(url=url, params=params, headers=self.headers, **kwargs)
+        return self.session.get(url=url, params=params, headers=self.headers, **kwargs)
 
     def delete(self, url, **kwargs):
-        return session.delete(url=url, headers=self.headers, **kwargs)
+        return self.session.delete(url=url, headers=self.headers, **kwargs)
 
     def post(self, url, data=None, json=None, **kwargs):
-        return session.post(url=url, data=data, json=json, headers=self.headers, **kwargs)
+        # pprint(f"url: {url}")
+        # pprint(f"data: {data}")
+        # pprint(f"json: {json}")
+        # pprint(f"headers: {self.headers}")
+        return self.session.post(url=url, data=data, json=json, headers=self.headers, **kwargs)
 
     def patch(self, url, data=None, json=None, **kwargs):
-        return session.patch(url=url, data=data, json=json, headers=self.headers, **kwargs)
+        return self.session.patch(url=url, data=data, json=json, headers=self.headers, **kwargs)
     
