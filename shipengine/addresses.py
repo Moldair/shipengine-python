a323a27e0f1edef9430085936d5b32d2553a0b1a
Mickey McClellan <288259+Moldair@users.noreply.github.com>
2021-08-01 16:14:19 -0500

    MVP for Addresses.

--- shipengine/addresses.py
+++ shipengine/addresses.py
@@ -1,125 +1,124 @@
-from shipengine import ShipEngine
+from requests.models import StreamConsumedError
+from shipengine import SE_SUCCESS, ShipEngine
+from pprint import pprint
+
 
 class Address(ShipEngine):
-    
+    STATUS_VALUES = {
+        "STATUS_UNVERIFIED": "unverified",
+        "STATUS_VERIFIED": "verified",
+        "STATUS_WARNING": "warning",
+        "STATUS_ERROR": "error",
+    }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._name = ''
-        '''
-        string non-empty
-        The name of a contact person at this address. This field may be set instead of - or in addition to - the company_name field.
-        '''
-        self._phone = ''
-        '''
-        string non-empty
-        The phone number of a contact person at this address. The format of this phone number varies depending on the country.
-        '''
-        self._company_name = ''
-        '''string non-empty Nullable
-        If this is a business address, then the company name should be specified here.
-        '''
-        self._address_line1 = ''
-        '''
-        required
-        string non-empty
-        The first line of the street address. For some addresses, this may be the only line. Other addresses may require 2 or 3 lines.
-        '''
-        self._address_line2 = ''
-        '''
-        string non-empty Nullable
-        The second line of the street address. For some addresses, this line may not be needed.
-        '''
-
-        self._address_line3 = ''
-        '''string non-empty Nullable
-        The third line of the street address. For some addresses, this line may not be needed.
-        '''
-
-        self._city_locality = ''
-        '''
-        required
-        string non-empty
-        The name of the city or locality
-        '''
-
-        self._state_province = ''
-        '''
-        required
-        string non-empty
-        The state or province. For some countries (including the U.S.) only abbreviations are allowed. Other countries allow the full name or abbreviation.
-        '''
-
-        self._postal_code = ''
-        '''
-        string non-empty
-        postal code
-        '''
-
-        self._country_code = ''
-        '''
-        required
-        string 2 characters
-        The two-letter ISO 3166-1 country code
-        '''
-
-        self._address_residential_indicator = ''
-        '''string
-        Default: "unknown"
-        Enum: "unknown" "yes" "no"
-        Indicates whether this is a residential address.
-        '''
+        self._status = ""
+        self._address = {
+            'name': '',
+                # string non-empty                                
+                # The name of a contact person at this address. 
+                # This field may be set instead of - or 
+                # in addition to - the company_name field.
+            'phone': '',                           
+                # string non-empty
+                # The phone number of a contact person at 
+                # this address. The format of this phone number 
+                # varies depending on the country.
+            'company_name': '',                   
+                # string non-empty Nullable
+                # If this is a business address, then 
+                # the company name should be specified here.
+            'address_line1': '',                    
+                # required
+                # string non-empty
+                # The first line of the street address. 
+                # For some addresses, this may be the only 
+                # line. Other addresses may require 2 or 3 lines.
+            'address_line2': '',                   
+                # string non-empty Nullable
+                # The second line of the street address. 
+                # For some addresses, this line may not be needed.
+            'address_line3': '',                   
+                # string non-empty Nullable
+                # The third line of the street address. 
+                # For some addresses, this line may not be needed.
+            'city_locality': '',                    
+                # required
+                # string non-empty
+                # The name of the city or locality
+            'state_province': '',                   
+                # required
+                # string non-empty
+                # The state or province. For some countries 
+                # (including the U.S.) only abbreviations are allowed. 
+                # Other countries allow the full name or abbreviation.
+            'postal_code': '',                   
+                # string non-empty
+                # postal code
+            'country_code': '',                     
+                # required
+                # string 2 characters
+                # The two-letter ISO 3166-1 country code
+            'address_residential_indicator': '',   
+                # string
+                # Default: "unknown"
+                # Enum: "unknown" "yes" "no"
+                # Indicates whether this is a residential address.
+        }
+        self._messages = []
         return
 
     @property
     def name(self):
-        return self._name
+        return self._address['name']
     
     @property
     def phone(self):
-        return self._phone
+        return self._address['phone']
     
     @property
     def company_name(self):
-        return self._company_name
+        return self._address['company_name']
     
     @property
     def address_line1(self):
-        return self._address_line1
+        return self._address['address_line1']
     
     @property
     def address_line2(self):
-        return self._address_line2
+        return self._address['address_line2']
     
     @property
     def address_line3(self):
-        return self._address_line3
+        return self._address['address_line3']
     
     @property
     def city_locality(self):
-        return self._city_locality
+        return self._address['city_locality']
     
     @property
     def state_province(self):
-        return self._state_province
+        return self._address['state_province']
     
     @property
     def postal_code(self):
-        return self._postal_code
+        return self._address['postal_code']
     
     @property
     def country_code(self):
-        return self._country_code
+        return self._address['country_code']
     
     @property
     def address_residential_indicator(self):
-        return self._address_residential_indicator
+        return self._address['address_residential_indicator']
     
+
     def __repr__(self):
-        return f"<Address: {self.address_line1}>"
+        return f"<Address: {self._address['address_line1']}>"
     
     def to_json(self):
-        return self.__dict__
+        return self._address
     
     def validate(self, address=None):
         '''
@@ -142,23 +141,55 @@ class Address(ShipEngine):
             )
         
         response = self.post(self.url+_endpoint, json=address)
-        return response
+        data = response.json()[0]
+        if response.status_code == SE_SUCCESS:
+            if data['status'] == self.STATUS_VALUES['STATUS_VERIFIED']:
+                self._status = data['status']
+                self._address = data['matched_address']
+                self._messages = data['messages']
+            else:
+                self._status = data['status']
+                self._address = data['original_address']
+                self._messages = data['messages']
+            return True
+        return False
 
     def parse(self, text=None, knowns={}):
         '''
         Parse Text string via ShipEngine API Call
+
         '''
         _endpoint = '/v1/addresses/recognize'
-        required_keys = [
-            'text',
-        ]
-        if not isinstance(text, dict) or 'text' not in text.keys():
-            raise Exception(
-                "The text parameter should be a dictionary.\n"
-                "Required keys: " + str(required_keys)
-            )
+        if not isinstance(text, str):
+            raise Exception("The text parameter must be a string.")
+        if text == "":
+            self._status = self.STATUS_VALUES['STATUS_ERROR']
+            self._messages.append("Text string is too short.")
+            return False
+        query = {
+            'text': text
+        }
+        query['address'] = knowns
+        # pprint(f"query: {query}")
+        response = self.put(self.url+_endpoint, json=query)
+        data = response.json()
+        # pprint(f"data: {data}")
+        if response.status_code == SE_SUCCESS:
+            if self.validate(address=[data['address']]):
+                return True
+            else:
+                self._status = self.STATUS_VALUES['STATUS_WARNING']
+                self._messages = ['Address validation failed.']
+                
+        else:
+            self._status = self.STATUS_VALUES['STATUS_ERROR']
+            self._messages.append(data['message'])
         
-        response = self.put(self.url+_endpoint, json=text)
+        return False
         
-        return response 
-        
+
+
+    def isverified(self):
+        if self._status == 'verified':
+            return True
+        return False
--- tests/test_Address.py
+++ tests/test_Address.py
@@ -7,149 +7,24 @@ import pytest
 import vcr
 import json
 
-@pytest.fixture
-def address():
+def test_Address_isverified():
     x = Address()
-    pprint(x.headers)
-    return x
+    x._status = 'verified'
 
-@pytest.fixture
-def parse_address():
-    return {
-        "text": "Margie McMiller at 3800 North Lamar suite 200 in austin, tx.  The zip code there is 78652."
-    }
+    assert x.isverified()
 
-@pytest.fixture
-def parse_expected_keys():
-    data = {
-        "score": 0.9122137426845613,
-        "address": {
-            "name": "Margie McMiller",
-            "address_line1": "3800 North Lamar",
-            "address_line2": "Suite 200",
-            "city_locality": "Austin",
-            "state_province": "TX",
-            "postal_code": 78652,
-            "address_residential_indicator": "unknown"
-        },
-        "entities": [
-            {
-            "type": "person",
-            "score": 0.9519646137063122,
-            "text": "Margie McMiller",
-            "start_index": 0,
-            "end_index": 14,
-            "result": {
-                "value": "Margie McMiller"
-            }
-            },
-            {
-            "type": "address_line",
-            "score": 0.9805313966503588,
-            "text": "3800 North Lamar",
-            "start_index": 19,
-            "end_index": 34,
-            "result": {
-                "line": 1,
-                "value": "3800 North Lamar"
-            }
-            },
-            {
-            "type": "number",
-            "score": 0.9805313966503588,
-            "text": 3800,
-            "start_index": 19,
-            "end_index": 22,
-            "result": {
-                "type": "cardinal",
-                "value": 3800
-            }
-            },
-            {
-            "type": "address_line",
-            "score": 1,
-            "text": "suite 200",
-            "start_index": 36,
-            "end_index": 44,
-            "result": {
-                "line": 2,
-                "value": "Suite 200"
-            }
-            },
-            {
-            "type": "number",
-            "score": 0.9805313966503588,
-            "text": 200,
-            "start_index": 42,
-            "end_index": 44,
-            "result": {
-                "type": "cardinal",
-                "value": 200
-            }
-            },
-            {
-            "type": "city_locality",
-            "score": 0.9805313966503588,
-            "text": "austin",
-            "start_index": 49,
-            "end_index": 54,
-            "result": {
-                "value": "Austin"
-            }
-            },
-            {
-            "type": "state_province",
-            "score": 0.6082904353940255,
-            "text": "tx",
-            "start_index": 57,
-            "end_index": 58,
-            "result": {
-                "name": "Texas",
-                "value": "TX"
-            }
-            },
-            {
-            "type": "postal_code",
-            "score": 0.9519646137063122,
-            "text": 78652,
-            "start_index": 84,
-            "end_index": 88,
-            "result": {
-                "value": 78652
-            }
-            }
-        ]
-    }
-    return data.keys()
-@vcr.use_cassette('tests/vcr_cassettes/Address/parse-address.yml', filter_query_parameters=['api_key'], filter_headers=['API-Key'])
-def test_Parse_Address(address, parse_address, parse_expected_keys):
-    r = address.parse(parse_address)
+def test_Address_isnotverified():
+    x = Address()
+    x._status = 'unverified'
 
-    if r.status_code != SE_SUCCESS:
-        pprint(r.json())
-    assert r.status_code == SE_SUCCESS
-    if not set(parse_expected_keys).issubset(r.json().keys()):
-        pprint(set(parse_expected_keys).difference(r.json().keys()))
-    assert set(parse_expected_keys).issubset(r.json()), "All keys should be present"
+    assert not x.isverified()
 
-def test_Parse_Address_malformed_input(address):
-    expected_exception = "The text parameter should be a dictionary."
-    with pytest.raises(Exception) as excinfo:
-        r = address.parse(text=None)
-    assert expected_exception in str(excinfo.value)
-    
-    with pytest.raises(Exception) as excinfo:
-        r = address.parse(text="")
-    assert expected_exception in str(excinfo.value)
-    
-    with pytest.raises(Exception) as excinfo:
-        r = address.parse(text={})
-    assert expected_exception in str(excinfo.value)
-    
 
-@pytest.fixture
-def validate_address():
-    data = [
+@vcr.use_cassette('tests/vcr_cassettes/Address/validate-address.yml', 
+                    filter_query_parameters=['api_key'], 
+                    filter_headers=['API-Key'])
+def test_validate_address():
+    test_address = [
         {
             "name": "Mickey and Minnie Mouse",
             "phone": "714-781-4565",
@@ -161,66 +36,139 @@ def validate_address():
             "country_code": "US"
         }
     ]
-    return data
 
-@pytest.fixture
-def validate_expected_keys():
-    data = [
-        {
-            "status": "verified",
-            "original_address": {
-            "name": "Mickey and Minnie Mouse",
-            "phone": "714-781-4565",
-            "company_name": "The Walt Disney Company",
-            "address_line1": "500 South Buena Vista Street",
-            "address_line2": "null",
-            "address_line3": "null",
-            "city_locality": "Burbank",
-            "state_province": "CA",
-            "postal_code": 91521,
-            "country_code": "US",
-            "address_residential_indicator": "unknown"
-            },
-            "matched_address": {
-            "name": "MICKEY AND MINNIE MOUSE",
-            "phone": "714-781-4565",
-            "company_name": "THE WALT DISNEY COMPANY",
-            "address_line1": "500 S BUENA VISTA ST",
-            "address_line2": "null",
-            "address_line3": "null",
-            "city_locality": "BURBANK",
-            "state_province": "CA",
-            "postal_code": "91521-0007",
-            "country_code": "US",
-            "address_residential_indicator": "no"
-            },
-            "messages": []
-        }
-    ]
-    return data[0].keys()
+    sut = Address()
+    sut.validate(address=test_address)
 
-@vcr.use_cassette('tests/vcr_cassettes/Address/validate-address.yml', filter_query_parameters=['api_key'], filter_headers=['API-Key'])
-def test_Validate_Address(address, validate_address, validate_expected_keys):
-    r = address.validate(address=validate_address)
+    assert sut.isverified()
 
-    if r.status_code != SE_SUCCESS:
-        pprint(r.json())
-    assert r.status_code == SE_SUCCESS
-    if not set(validate_expected_keys).issubset(r.json()[0].keys()):
-        pprint(set(validate_expected_keys).difference(r.json()[0].keys()))
-    assert set(validate_expected_keys).issubset(r.json()[0].keys()), "All keys should be present."
-
-def test_Validate_Address_malformed_address(address):
-    expected_exception = "The address parameter should be a list containing a dictionary."
-    with pytest.raises(Exception) as excinfo:
-        r = address.validate(address=None)
-    assert expected_exception in str(excinfo.value)
+@vcr.use_cassette('tests/vcr_cassettes/Address/parse-address.yml', 
+                    filter_query_parameters=['api_key'], 
+                    filter_headers=['API-Key'])
+def test_Address_parse_address():
+    test_address = "Margie McMiller at 3800 North Lamar suite 200 in austin, tx.  The zip code there is 78652."
     
-    with pytest.raises(Exception) as excinfo:
-        r = address.validate(address="")
-    assert expected_exception in str(excinfo.value)
+    sut = Address()
+    sut.parse(text=test_address)
+
+    assert not sut.isverified()  # The test address is an unverifiable address.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#     r = address.parse(parse_address)
+
+#     if r.status_code != SE_SUCCESS:
+#         pprint(r.json())
+#     assert r.status_code == SE_SUCCESS
+#     if not set(parse_expected_keys).issubset(r.json().keys()):
+#         pprint(set(parse_expected_keys).difference(r.json().keys()))
+#     assert set(parse_expected_keys).issubset(r.json()), "All keys should be present"
+
+# def test_Parse_Address_malformed_input(address):
+#     expected_exception = "The text parameter should be a string."
+#     inputs = [None, {}, [], ()]
+#     for input in inputs:
+#         with pytest.raises(Exception) as excinfo:
+#             r = address.parse(text=input)
+#         assert expected_exception in str(excinfo.value)
+        
+
+# @pytest.fixture
+# def validate_address():
+#     data = [
+#         {
+#             "name": "Mickey and Minnie Mouse",
+#             "phone": "714-781-4565",
+#             "company_name": "The Walt Disney Company",
+#             "address_line1": "500 South Buena Vista Street",
+#             "city_locality": "Burbank",
+#             "state_province": "CA",
+#             "postal_code": "91521",
+#             "country_code": "US"
+#         }
+#     ]
+#     return data
+
+# @pytest.fixture
+# def validate_expected_keys():
+#     data = [
+#         {
+#             "status": "verified",
+#             "original_address": {
+#             "name": "Mickey and Minnie Mouse",
+#             "phone": "714-781-4565",
+#             "company_name": "The Walt Disney Company",
+#             "address_line1": "500 South Buena Vista Street",
+#             "address_line2": "null",
+#             "address_line3": "null",
+#             "city_locality": "Burbank",
+#             "state_province": "CA",
+#             "postal_code": 91521,
+#             "country_code": "US",
+#             "address_residential_indicator": "unknown"
+#             },
+#             "matched_address": {
+#             "name": "MICKEY AND MINNIE MOUSE",
+#             "phone": "714-781-4565",
+#             "company_name": "THE WALT DISNEY COMPANY",
+#             "address_line1": "500 S BUENA VISTA ST",
+#             "address_line2": "null",
+#             "address_line3": "null",
+#             "city_locality": "BURBANK",
+#             "state_province": "CA",
+#             "postal_code": "91521-0007",
+#             "country_code": "US",
+#             "address_residential_indicator": "no"
+#             },
+#             "messages": []
+#         }
+#     ]
+#     return data[0].keys()
+
+# @vcr.use_cassette('tests/vcr_cassettes/Address/validate-address.yml', filter_query_parameters=['api_key'], filter_headers=['API-Key'])
+# def test_Validate_Address(address, validate_address, validate_expected_keys):
+#     r = address.validate(address=validate_address)
+
+#     if r.status_code != SE_SUCCESS:
+#         pprint(r.json())
+#     assert r.status_code == SE_SUCCESS
+#     if not set(validate_expected_keys).issubset(r.json()[0].keys()):
+#         pprint(set(validate_expected_keys).difference(r.json()[0].keys()))
+#     assert set(validate_expected_keys).issubset(r.json()[0].keys()), "All keys should be present."
+
+# def test_Validate_Address_malformed_address(address):
+#     expected_exception = "The address parameter should be a list containing a dictionary."
+#     with pytest.raises(Exception) as excinfo:
+#         r = address.validate(address=None)
+#     assert expected_exception in str(excinfo.value)
     
-    with pytest.raises(Exception) as excinfo:
-        r = address.validate(address={})
-    assert expected_exception in str(excinfo.value)
+#     with pytest.raises(Exception) as excinfo:
+#         r = address.validate(address="")
+#     assert expected_exception in str(excinfo.value)
+    
+#     with pytest.raises(Exception) as excinfo:
+#         r = address.validate(address={})
+#     assert expected_exception in str(excinfo.value)
     
