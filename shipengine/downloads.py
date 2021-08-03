e10bcb13a1e1c36ec2203438d2abe16887c8bde0
Mickey McClellan <288259+Moldair@users.noreply.github.com>
2021-08-02 23:31:09 -0500

    refactored get functions

--- shipengine/downloads.py
+++ shipengine/downloads.py
@@ -13,58 +13,39 @@ class Download(ShipEngine):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
-    def download_url(self, url=None):
-        _endpoint = "/v1/downloads"
-        if isinstance(url, str):
-            if "https://api.shipengine.com/v1/downloads" not in url:
-                raise ImproperDownloadParameter
-            filespec = url.replace(self.url+_endpoint, '')
-            split_filespec = filespec.split('/')
-            if split_filespec[0] == "":
-                split_filespec = split_filespec[1:]
 
-            pprint(split_filespec)
-            if len(split_filespec) == 3:
-                filespec = "/" + "/".join(split_filespec)
-            else:
-                raise ImproperDownloadParameter("download parameter should be of form /dir/subdir/filename")
-
-
-            self.response = self.get(url=self.url+_endpoint+filespec)
-            pprint(self.response.json())
-            if self.response.status_code == SE_SUCCESS:
+    def _check_response(self):
+        if self.response.status_code == SE_SUCCESS:
                 self._file = self.response.text
                 return True
-            elif self.response.status_code == SE_BAD_REQUEST:
-                raise FileNotFound404
-            elif self.response.status_code == SE_NOT_FOUND:
-                raise FileNotFound404
+        elif self.response.status_code == SE_BAD_REQUEST:
+            raise FileNotFound404
+        elif self.response.status_code == SE_NOT_FOUND:
+            raise FileNotFound404
+        
+    def _get_file(self, filespec=None):
+        _endpoint = "/v1/downloads"
+        split_filespec = filespec.split('/')
+        if split_filespec[0] == "":
+            split_filespec = split_filespec[1:]
+        if len(split_filespec) == 3:
+            filespec = "/" + "/".join(split_filespec)
+        else:
+            raise ImproperDownloadParameter("download parameter should be of form /dir/subdir/filename")
+        self.response = self.get(url=self.url+_endpoint+filespec)
+
+    def download_url(self, url=None):
+        _endpoint = "https://api.shipengine.com/v1/downloads"
+        if isinstance(url, str):
+            if _endpoint not in url:
+                raise ImproperDownloadParameter
+            filespec = url.replace(_endpoint, '')
+            self._get_file(filespec)
+            return self._check_response()
         return False
 
     def download_file(self, filespec=None):
-        _endpoint = "/v1/downloads"
         if isinstance(filespec, str):
-            split_filespec = filespec.split('/')
-            if split_filespec[0] == "":
-                split_filespec = split_filespec[1:]
-
-            pprint(split_filespec)
-            if len(split_filespec) == 3:
-                filespec = "/" + "/".join(split_filespec)
-            else:
-                raise ImproperDownloadParameter("download parameter should be of form /dir/subdir/filename")
-
-            self.response = self.get(url=self.url+_endpoint+filespec)
-            # pprint(self.response.status_code)
-            # pprint(filespec)
-            # pprint(self.url+_endpoint+filespec)
-            # pprint(self.response.text)
-            if self.response.status_code == SE_SUCCESS:
-                self._file = self.response.text
-                return True
-            elif self.response.status_code == SE_BAD_REQUEST:
-                raise FileNotFound404
-            elif self.response.status_code == SE_NOT_FOUND:
-                raise FileNotFound404
+            self._get_file(filespec)
+            return self._check_response()
         return False
